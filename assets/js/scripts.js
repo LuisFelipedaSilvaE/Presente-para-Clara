9ad9c99d5d1c4f98334fd69a0b19377bb199c90f
LuisFelipedaSilvaE <luisfelipe070123@gmail.com>
2025-08-19 16:33:11 -0300

    Corrige as interações feitas no javascript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,18 @@
 document.querySelector('h1#logo').addEventListener('click', () => {
-  document.querySelector('input#close-menu').checked = false;
+  document.querySelector('aside.menu').classList.toggle('toggle-menu');
+  document.querySelector('div#mobile-menu').classList.toggle('x-menu');
+  document.querySelector('div#mobile-menu').classList.toggle('hamburguer-menu');
+
 });
 
 document.querySelector('ul#navigation').addEventListener('click', () => {
-  document.querySelector('input#close-menu').checked = false;
+  document.querySelector('aside.menu').classList.toggle('toggle-menu');
+  document.querySelector('div#mobile-menu').classList.toggle('x-menu');
+  document.querySelector('div#mobile-menu').classList.toggle('hamburguer-menu');
+});
+
+document.querySelector('div.close-menu').addEventListener('click', () => {
+  document.querySelector('aside.menu').classList.toggle('toggle-menu');
+  document.querySelector('div#mobile-menu').classList.toggle('hamburguer-menu');
+  document.querySelector('div#mobile-menu').classList.toggle('x-menu');
 });

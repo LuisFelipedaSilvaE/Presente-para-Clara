07671c3a4aeec15c8baa1f4f3ae84424cda2b8f5
Luis Felipe <2511550630@fema.edu.br>
2025-10-21 15:05:45 -0300

    Refatora "card" para dialog e implementa melhorias

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,64 +1,75 @@
-document.querySelector('div#mobile-menu').addEventListener('click', () => {
-  if(document.querySelector('.card-container') !== null) {
-    document.querySelector('.card-container').style.display = 'none';
-  }  
-});
-
-document.querySelector('h1#logo').addEventListener('click', () => {
-  document.querySelector('aside.menu').classList.toggle('toggle-menu');
-  document.querySelector('div#mobile-menu').classList.toggle('x-menu');
-  document.querySelector('div#mobile-menu').classList.toggle('hamburguer-menu');
-  if(document.querySelector('.card-container') !== null) {
-    document.querySelector('.card-container').style.display = 'none';
+document.querySelector("div#mobile-menu").addEventListener("click", () => {
+  if (document.querySelector(".dialog-container") !== null) {
+    document.querySelector(".dialog-container").style.display = "none";
   }
 });
 
-document.querySelector('ul#navigation').addEventListener('click', () => {
-  document.querySelector('aside.menu').classList.toggle('toggle-menu');
-  document.querySelector('div#mobile-menu').classList.toggle('x-menu');
-  document.querySelector('div#mobile-menu').classList.toggle('hamburguer-menu');
-  if(document.querySelector('.card-container') !== null) {
-    document.querySelector('.card-container').style.display = 'none';
+document.querySelector("h1#logo").addEventListener("click", () => {
+  document.querySelector("aside.menu").classList.toggle("toggle-menu");
+  document.querySelector("div#mobile-menu").classList.toggle("x-menu");
+  document.querySelector("div#mobile-menu").classList.toggle("hamburguer-menu");
+  if (document.querySelector(".dialog-container") !== null) {
+    document.querySelector(".dialog-container").style.display = "none";
   }
 });
 
-document.querySelector('div.close-menu').addEventListener('click', () => {
-  document.querySelector('aside.menu').classList.toggle('toggle-menu');
-  document.querySelector('div#mobile-menu').classList.toggle('hamburguer-menu');
-  document.querySelector('div#mobile-menu').classList.toggle('x-menu');
+document.querySelector("ul#navigation").addEventListener("click", () => {
+  document.querySelector("aside.menu").classList.toggle("toggle-menu");
+  document.querySelector("div#mobile-menu").classList.toggle("x-menu");
+  document.querySelector("div#mobile-menu").classList.toggle("hamburguer-menu");
+  if (document.querySelector(".dialog-container") !== null) {
+    document.querySelector(".dialog-container").style.display = "none";
+  }
 });
 
-document.querySelector('#close-card').addEventListener('click', () => {
-  document.querySelector('.card-container').style.opacity = 0;
+document.querySelector("div.close-menu").addEventListener("click", () => {
+  document.querySelector("aside.menu").classList.toggle("toggle-menu");
+  document.querySelector("div#mobile-menu").classList.toggle("hamburguer-menu");
+  document.querySelector("div#mobile-menu").classList.toggle("x-menu");
+});
+
+document.querySelector("#close-dialog").addEventListener("click", () => {
+  document.querySelector(".dialog-container").style.opacity = 0;
   setTimeout(() => {
-    document.querySelector('.card-container').style.display = 'none';
+    document.querySelector(".dialog-container").style.display = "none";
   }, 300);
-  document.querySelector('.card').style.scale = 0;
+  document.querySelector(".dialog").style.scale = 0;
 });
 
-document.querySelectorAll('.skin-cell').forEach((img, index) => {  
-  img.addEventListener('click', () => {
-  let cardColors = ['rgb(51, 0, 85)', 'rgb(15, 195, 195)', 'rgb(255, 65, 95)', 'rgb(255, 175, 100)', 'rgb(220, 195, 220)', 'rgb(135, 120, 255)', 'rgb(255, 160, 205)']
-  let cardSpan = [img.dataset.rarity, img.dataset.limited]
-  let src = img.getAttribute('src');
-  let alt = img.getAttribute('alt');
+document.querySelectorAll(".skin-cell").forEach((img, index) => {
+  img.addEventListener("click", () => {
+    let dialogColors = [
+      "rgb(51, 0, 85)",
+      "rgb(15, 195, 195)",
+      "rgb(255, 65, 95)",
+      "rgb(255, 175, 100)",
+      "rgb(220, 195, 220)",
+      "rgb(135, 120, 255)",
+      "rgb(255, 160, 205)",
+    ];
+    let dialogSpan = [img.dataset.rarity, img.dataset.limited];
+    let src = img.getAttribute("src");
+    let alt = img.getAttribute("alt");
 
-  document.querySelector('.card-container').style.color = cardColors[index];
-  document.querySelector('.card-container').style.display = 'flex';
-  document.querySelector('.card-container').style.opacity = 100;
-  document.querySelector('.card-container').style.scale = 1;
+    document.querySelector(".dialog-container").style.color =
+      dialogColors[index];
+    document.querySelector(".dialog-container").style.display = "flex";
+    document.querySelector(".dialog-container").style.opacity = 100;
+    document.querySelector(".dialog-container").style.scale = 1;
 
-  document.querySelector('.card').style.display = 'flex';
-  document.querySelector('.card').style.opacity = 100;
-  document.querySelector('.card').style.scale = 1;
+    document.querySelector(".dialog").style.display = "flex";
+    document.querySelector(".dialog").style.opacity = 100;
+    document.querySelector(".dialog").style.scale = 1;
 
-  document.querySelector('.card-heading h3').innerHTML = img.dataset.title;
-  
-  document.querySelectorAll('.card-content > h4 > span').forEach((span, i) => {
-    span.textContent = cardSpan[i];
-  });;
-  
-    document.querySelector('#card-img').setAttribute('src', src);
-    document.querySelector('#card-img').setAttribute('alt', alt);
+    document.querySelector(".dialog-heading h3").innerHTML = img.dataset.title;
+
+    document
+      .querySelectorAll(".dialog-content > h4 > span")
+      .forEach((span, i) => {
+        span.textContent = dialogSpan[i];
+      });
+
+    document.querySelector("#dialog-img").setAttribute("src", src);
+    document.querySelector("#dialog-img").setAttribute("alt", alt);
   });
 });
